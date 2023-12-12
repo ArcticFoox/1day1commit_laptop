af03ea1200b5e6c105e1dc888cc1b8b10316f03f
shinjongshin <jongsin16@naver.com>
2023-12-12 21:08:20 +0900

    2798
    brute force
    https://www.acmicpc.net/problem/2798

---
+++ blackjack.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n, m, sum = 0, result;
+    int min = 9999999;
+    int arr[101] = {0, };
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n-2; i++)
+        for(int j = i+1; j < n-1; j++)
+            for(int k = j+1; k < n; k++)
+            {
+                sum = arr[i]+arr[j]+arr[k];
+                if(m - sum < min && m - sum >= 0) {
+                    min = m - sum;
+                    result = sum;
+                }
+            }
+    cout << result;
+    return 0;
+}
